ce5fdc746b7386fecae5f2561b31b6b7e3d775dd
Rishik Sahu <rishikkr@hotmail.com>
2023-07-02 07:48:50 +0530

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const whitelist = ['https://www.rishiksahu.in/', 'http://localhost:3000', 'http://localhost:3000']
+const whitelist = ['https://www.rishiksahu.in/', 'http://localhost:3000', 'http://localhost:3000', 'https://paper-trade-backend.vercel.app/']
 
 module.exports = whitelist
